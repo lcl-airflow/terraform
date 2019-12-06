ab668872f50a709fc07792c0a7ce0853fc7eb584
lcl-airflow <herman.cheung@loblaw.ca>
2019-12-06 15:42:14 -0500

    terraform

---
+++ main.tf
@@ -0,0 +1,29 @@
+// Configure the Google Cloud provider
+provider "google" {
+ credentials = "${file("CREDENTIALS_FILE.json")}"
+ project     = "playground-s-11-7c616e"
+ region      = "us-east1"
+}
+
+
+resource "google_composer_environment" "test" {
+  name   = "lcl-data-management"
+  region = "us-central1"
+
+	config {
+    software_config {
+      airflow_config_overrides = {
+        core-load_example = "True"
+      }
+
+      pypi_packages = {
+        numpy = ""
+        scipy = "==1.1.0"
+        fastavro = ""
+        avro = ""
+      }
+    }
+  }
+}
+
+
